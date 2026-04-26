209ebbee8947118052e3d2c66f120e748c0ae0b1
18-Madhav-9 <159808252+18-Madhav-9@users.noreply.github.com>
2026-04-26 15:31:46 +0530

    CPU monitoring function that calculates usage by comparing FILETIME values from GetSystemTimes

---
+++ cpu.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <iostream>
+
+// coverting FILETIME struct to actual ticks 
+ULONGLONG filetimeToULL(const FILETIME &f ) {
+    return  (((ULONGLONG)f.dwHighDateTime << 32 ))|f.dwLowDateTime ;
+}
+
+void cpu() {
+    FILETIME idle1,kernel1,user1;
+    FILETIME idle2,kernel2,user2 ;
+
+    GetSystemTimes(&idle1,&kernel1,&user1);
+    Sleep(1000);
+    GetSystemTimes(&idle2,&kernel2,&user2) ;
+
+    ULONGLONG idle = filetimeToULL(idle2)-filetimeToULL(idle1);
+    ULONGLONG kernel = filetimeToULL(kernel2)-filetimeToULL(kernel1);
+    ULONGLONG user = filetimeToULL(user2)-filetimeToULL(user1) ;
+
+    double usage =  (1.0 - (double)idle/(kernel+user))*100 ;
+    
+    std::cout << "CPU Usage : " << usage << "%\n" ;
+}
+
+int main() {
+
+    cpu() ;
+    return 0 ;
+}
