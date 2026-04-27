6fabe52617a6de1f2ce132d889a1a917a15ffa61
18-Madhav-9 <159808252+18-Madhav-9@users.noreply.github.com>
2026-04-27 09:54:07 +0530

    Updated process to get process list with memory usage

--- process.cpp
+++ process.cpp
@@ -1,21 +1,53 @@
 #include <windows.h>
 #include <tlhelp32.h>
+#include <psapi.h>
 #include <iostream>
+#include <vector>
 
-void process(){
+// convert btyes to MB ;
+#define MB (1024*1024)
+
+
+struct ProcessInfo {
+    std::string name;
+    DWORD id ;
+    double memoryMb;
+};
+
+std::vector<ProcessInfo> process(){
 
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0) ;
 
     PROCESSENTRY32 current ;
     current.dwSize = sizeof(current) ;
 
+    std::vector<ProcessInfo> plist;
+
     if (Process32First(snapshot,&current)) {
-        while ( Process32Next(snapshot,&current) ) {
-            std::wcout << "Process Name : "<<current.szExeFile << "\n" ;
-            std::wcout << "Process Id : " << current.th32ProcessID << "\n" ;
-        }
+        int count = 0 ;
+        do {
+            ProcessInfo p ;
+            p.name = current.szExeFile ;
+            p.id = current.th32ProcessID ;
+            p.memoryMb = 0.0 ;
+
+            if ( p.id <=10 ) continue ;//skip some system processess
+
+            HANDLE hprocess = OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ , false,p.id) ;
+
+            PROCESS_MEMORY_COUNTERS pmc ;
+            if ( GetProcessMemoryInfo(hprocess,&pmc,sizeof(pmc)) ) {
+                p.memoryMb = pmc.WorkingSetSize/MB ;
+            }
+            CloseHandle(hprocess) ;
+
+            plist.push_back(p) ;
+            count++ ;
+            if ( count >=5 ) break ;
+        }while ( Process32Next(snapshot,&current) ) ;
     }
     
     CloseHandle(snapshot) ;
 
+    return plist ;
 }
